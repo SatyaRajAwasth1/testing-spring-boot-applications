b3eeafb7b2942672ac06bb738d5dc02ff1065833
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2023-04-21 22:50:15 +0545

    [#1]create project skeleton & health status up

---
+++ src/main/java/np/com/satyarajawasthi/testingspringappsample/TestingSpringAppSampleApplication.java
@@ -0,0 +1,13 @@
+package np.com.satyarajawasthi.testingspringappsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingSpringAppSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingSpringAppSampleApplication.class, args);
+    }
+
+}
